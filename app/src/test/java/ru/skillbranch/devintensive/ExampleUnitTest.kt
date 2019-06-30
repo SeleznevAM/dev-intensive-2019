55a7a66202f213495227d195e78544386f5ca92f
ASeleznev <seleznevam84@ya.ru>
2019-06-30 11:28:46 +0300

    create User class

---
+++ app/src/test/java/ru/skillbranch/devintensive/ExampleUnitTest.kt
@@ -0,0 +1,36 @@
+package ru.skillbranch.devintensive
+
+import org.junit.Test
+
+import org.junit.Assert.*
+import ru.skillbranch.devintensive.models.User
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+
+    @Test
+    fun test_instance(){
+        val user = User("1")
+        val user2 = User("2")
+        val user3 = User("3")
+
+        println("$user $user2 $user3")
+    }
+
+    @Test
+    fun factory_test(){
+        val user = User.makeUser("John Cena")
+        val user1 = User.makeUser("")
+        val user2 = User.makeUser(null)
+        val user3 = User.makeUser("John ")
+        val user4 = User.makeUser("John")
+    }
+}
